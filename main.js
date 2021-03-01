4ed622ad491c77b2bd91830efbb573d44267728a
Maja Mrozińska <79254924+majuszko@users.noreply.github.com>
2021-03-01 16:36:59 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('.menu-toggler').on('click', function() {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+
+    $('.nav-link').on('click', function() {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+    $('nav a[href*="#"]').on('click', function(){
+        $('html,body').animate({scrollTop: $($(this).attr('href')).offset().top-100}, 2000);
+    });
+    $('#up').on('click', function() {
+        
+        $('html,body').animate({scrollTop:0},2000);
+    });
+
+
+});
